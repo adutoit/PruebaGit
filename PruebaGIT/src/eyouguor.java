f8f3ba95fc9ef4e4938dbafff44404df504de57c
adutoit <dutoit.a@gmail.com>
2012-05-11 09:43:17 +0200

    popo2

--- PruebaGIT/src/eyouguor.java
+++ PruebaGIT/src/eyouguor.java
@@ -3,5 +3,6 @@ public class eyouguor {
 public static void main(String[] args) {
 	System.out.println("hellooooo worrrrrrdllll!!!!");
 	System.out.println("cambio del copon");
+	System.out.println("acuca mare area area");
 }
 }
